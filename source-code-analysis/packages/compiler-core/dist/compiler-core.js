fdb71e34e1345d08cdeeee90dd903389d3794671
shower <leeshower2023@163.com>
2025-02-27 14:15:44 +0800

    ast语法转换，将模板转为str语法树

--- source-code-analysis/packages/compiler-core/dist/compiler-core.js
+++ source-code-analysis/packages/compiler-core/dist/compiler-core.js
@@ -1,1 +1,224 @@
+// packages/compiler-core/src/index.ts
+function createParseContext(content) {
+  return {
+    originalSource: content,
+    source: content,
+    //字符串不停的截取
+    line: 1,
+    //第一行
+    column: 1,
+    //第一列
+    offset: 0
+    //偏移量
+  };
+}
+function isEnd(context) {
+  const c = context.source;
+  if (c.startsWith("</")) {
+    return true;
+  }
+  return !context.source;
+}
+function advancePositionMutation(context, c, endIndex) {
+  let linesCount = 0;
+  let linePos = -1;
+  for (let i = 0; i < endIndex; i++) {
+    if (c.charCodeAt(i) == 10) {
+      linesCount++;
+      linePos = i;
+    }
+  }
+  context.offset += endIndex;
+  context.line += linesCount;
+  context.column = linePos == -1 ? context.column + endIndex : endIndex - linePos;
+}
+function advanceBy(context, endIndex) {
+  let c = context.source;
+  advancePositionMutation(context, c, endIndex);
+  context.source = c.slice(endIndex);
+}
+function parseTextData(context, endIndex) {
+  const rawText = context.source.slice(0, endIndex);
+  advanceBy(context, endIndex);
+  return rawText;
+}
+function parseText(context) {
+  let tokens = ["<", "{{"];
+  let endIndex = context.source.length;
+  for (let i = 0; i < tokens.length; i++) {
+    const index = context.source.indexOf(tokens[i], 1);
+    if (index !== -1 && index < endIndex) {
+      endIndex = index;
+    }
+  }
+  let start = getCursor(context);
+  return {
+    type: 2 /* TEXT */,
+    start,
+    content: parseTextData(context, endIndex)
+  };
+}
+function advanceSpaces(context) {
+  const match = /^[ \t\r\n]+/.exec(context.source);
+  if (match) {
+    advanceBy(context, match[0].length);
+  }
+}
+function getCursor(context) {
+  let { line, column, offset } = context;
+  return { line, column, offset };
+}
+function getSelection(context, start, e) {
+  let end = e || getCursor(context);
+  return {
+    start,
+    end,
+    source: context.originalSource.slice(start.offset, end.offset)
+  };
+}
+function parseAttributeValue(context) {
+  let quote = context.source[0];
+  const isQuoted = quote === '"' || quote === "'";
+  let content;
+  if (isQuoted) {
+    advanceBy(context, 1);
+    const endIndex = context.source.indexOf(quote, 1);
+    content = parseTextData(context, endIndex);
+    advanceBy(context, 1);
+  } else {
+    content = context.source.match(/([^ \t\r\n/>])+/)[1];
+    advanceBy(context, content.length);
+    advanceSpaces(context);
+  }
+  return content;
+}
+function parseAttribute(context) {
+  const start = getCursor(context);
+  let match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source);
+  const name = match[0];
+  let value;
+  advanceBy(context, name.length);
+  if (/^[\t\r\n\f ]*=/.test(context.source)) {
+    advanceSpaces(context);
+    advanceBy(context, 1);
+    advanceSpaces(context);
+    value = parseAttributeValue(context);
+  }
+  let loc = getSelection(context, start);
+  return {
+    type: 6 /* ATTRIBUTE */,
+    name,
+    value: {
+      type: 2 /* TEXT */,
+      content: value,
+      loc
+    },
+    loc: getSelection(context, start)
+  };
+}
+var parseAtrributes = (context) => {
+  const props = [];
+  while (context.source.length > 0 && !context.source.startsWith(">")) {
+    props.push(parseAttribute(context));
+    advanceSpaces(context);
+  }
+  return props;
+};
+function parseTag(context) {
+  const start = getCursor(context);
+  const match = /^<\/?([a-z][^ \t\r\n/>]*)/.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceSpaces(context);
+  let props = parseAtrributes(context);
+  const isSelfClosing = context.source.startsWith("/>");
+  advanceBy(context, isSelfClosing ? 2 : 1);
+  return {
+    type: 1 /* ELEMENT */,
+    tag,
+    isSelfClosing,
+    loc: getSelection(context, start),
+    // 开头标签解析后的信息
+    props
+  };
+}
+function parseElement(context) {
+  const ele = parseTag(context);
+  const children = parseChildren(context);
+  if (context.source.startsWith("</")) {
+    parseTag(context);
+  }
+  ele.children = children;
+  ele.loc = getSelection(context, ele.loc.start);
+  return ele;
+}
+function parseInterpolation(context) {
+  const start = getCursor(context);
+  const closeIndex = context.source.indexOf("}}", 2);
+  advanceBy(context, 2);
+  const innerStart = getCursor(context);
+  const innerEnd = getCursor(context);
+  const preTrimContent = parseTextData(context, closeIndex - 2);
+  const content = preTrimContent.trim();
+  const startOffset = preTrimContent.indexOf(content);
+  if (startOffset > 0) {
+    advancePositionMutation(innerStart, preTrimContent, startOffset);
+  }
+  const endOffset = startOffset + content.length;
+  advancePositionMutation(innerEnd, preTrimContent, endOffset);
+  advanceBy(context, 2);
+  return {
+    type: 5 /* INTERPOLATION */,
+    content: {
+      type: 4 /* SIMPLE_EXPRESSION */,
+      isStatic: false,
+      isConstant: false,
+      content,
+      loc: getSelection(context, innerStart, innerEnd)
+    },
+    loc: getSelection(context, start)
+  };
+}
+function parseChildren(context) {
+  const nodes = [];
+  while (!isEnd(context)) {
+    const c = context.source;
+    let node;
+    if (c.startsWith("{{")) {
+      node = parseInterpolation(context);
+    } else if (c[0] === "<") {
+      node = parseElement(context);
+    } else {
+      node = parseText(context);
+    }
+    nodes.push(node);
+  }
+  for (let i = 0; i < nodes.length; i++) {
+    let node = nodes[i];
+    if (node.type === 2 /* TEXT */) {
+      if (!/[^\t\r\n\f ]/.test(node.content)) {
+        nodes[i] = null;
+      } else {
+        node.content = node.content.replace(/[\t\r\n\f ]+/g, " ");
+      }
+    }
+  }
+  return nodes.filter(Boolean);
+}
+function createRoot(children) {
+  return {
+    type: 0 /* ROOT */,
+    children
+  };
+}
+function parse(template) {
+  const context = createParseContext(template);
+  return createRoot(parseChildren(context));
+}
+function compile() {
+}
+export {
+  compile,
+  parse
+};
 //# sourceMappingURL=compiler-core.js.map
